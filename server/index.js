d60a120e0208d093b52a3e2c98725d61ce8a401b
anirudh-bukka <anirudh.bukka@gmail.com>
2023-04-06 12:34:22 +0530

    All configurations.

--- server/index.js
+++ server/index.js
@@ -16,64 +16,58 @@
 // import { verifyToken } from "./middleware/auth.js";
 // import User from "./models/User.js";
 // import Post from "./models/Post.js";
-// // import { users, posts } from "./data/index.js";
+// import { users, posts } from "./data/index.js";
+// import { dirname } from 'path';
 
-// /* CONFIGURATIONS */
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// dotenv.config();
-// const app = express();
-// app.use(express.json());
-// app.use(helmet());
-// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-// app.use(morgan("common"));
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-// app.use(cors());
-// app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+// // /* CONFIGURATIONS */
+// // const __filename = fileURLToPath(import.meta.url);
+// // const __dirname = path.dirname(__filename);
+// // dotenv.config();
+// // const app = express();
+// // app.use(express.json());
+// // app.use(helmet());
+// // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// // app.use(morgan("common"));
+// // app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// // app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// // app.use(cors());
+// // app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
-// /* FILE STORAGE */
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/assets");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-// const upload = multer({ storage });
+// // /* FILE STORAGE */
+// // const storage = multer.diskStorage({
+// //   destination: function (req, file, cb) {
+// //     cb(null, "public/assets");
+// //   },
+// //   filename: function (req, file, cb) {
+// //     cb(null, file.originalname);
+// //   },
+// // });
+// // const upload = multer({ storage });
 
-// /* ROUTES WITH FILES */
-// app.post("/auth/register", upload.single("picture"), register);
-// app.post("/posts", verifyToken, upload.single("picture"), createPost);
+// // /* ROUTES WITH FILES */
+// // app.post("/auth/register", upload.single("picture"), register);
+// // app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-// /* ROUTES */
-// app.use("/auth", authRoutes);
-// app.use("/users", userRoutes);
-// app.use("/posts", postRoutes);
+// // /* ROUTES */
+// // app.use("/auth", authRoutes);
+// // app.use("/users", userRoutes);
+// // app.use("/posts", postRoutes);
 
-// /* MONGOOSE SETUP */
-// const PORT = process.env.PORT || 6001;
-// mongoose
-//   .connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+// // /* MONGOOSE SETUP */
+// // const PORT = process.env.PORT || 6001;
+// // mongoose
+// //   .connect(process.env.MONGO_URL, {
+// //     useNewUrlParser: true,
+// //     useUnifiedTopology: true,
+// //   })
+// //   .then(() => {
+// //     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-//     /* ADD DATA ONE TIME */
-//     // User.insertMany(users);
-//     // Post.insertMany(posts);
-//   })
-//   .catch((error) => console.log(`${error} did not connect`));
-
-
-
-
-
-
-
+// //     /* ADD DATA ONE TIME */
+// //     // User.insertMany(users);
+// //     // Post.insertMany(posts);
+// //   })
+// //   .catch((error) => console.log(`${error} did not connect`));
 
 
 
@@ -99,6 +93,7 @@ import { createPost } from "./controllers/posts.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import { verifyToken } from "./middleware/auth.js";
+import { dirname } from 'path';
 
 // CONFIGURATIONS --> all the middleware (something that runs between different requests/responses)
 const __filename = fileURLToPath(import.meta.url); // --> to grab the file url, specifically when we use modules
@@ -119,10 +114,19 @@ const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, "public/assets")
     },
+    // filename: function(req, file, cb) {
+    //     cb(null, file.originalname)
+    // }
     filename: function(req, file, cb) {
-        cb(null, file.originalname)
-    }
+        cb(null, Date.now() + file.originalname);
+      }
 });
+// // // configure multer storage
+// // const storage = multer.diskStorage({
+// //     filename: function(req, file, cb) {
+// //       cb(null, Date.now() + file.originalname);
+// //     }
+// //   });
 
 const upload = multer({ storage });
 
@@ -148,3 +152,44 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(() => {
     app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
 }).catch((error) => console.log(`${error} did not connect`));
+
+/*  ************************************ */
+
+// import express from "express";
+// const app = express();
+// import bodyParser from "body-parser";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import multer from "multer";
+// import helmet from "helmet";
+// import morgan from "morgan";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import authRoutes from "./routes/auth.js";
+// import userRoutes from "./routes/users.js";
+// import postRoutes from "./routes/posts.js";
+// import { register } from "./controllers/auth.js";
+// import { createPost } from "./controllers/posts.js";
+// import { verifyToken } from "./middleware/auth.js";
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { dirname } from 'path';
+
+
+// const port = process.env.PORT || 6001;
+
+// // Middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// // app.use(express.static(__dirname + '/public'));
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
