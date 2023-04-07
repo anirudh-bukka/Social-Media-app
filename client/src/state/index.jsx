1f6e277c120d13ca4cc99b476ed9aaf8e6335b05
anirudh-bukka <anirudh.bukka@gmail.com>
2023-04-07 10:28:17 +0530

    This will have all the logics required for the application

---
+++ client/src/state/index.jsx
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { // this data will be accessible throughout entire app --> we don't have to explicitly pass in state and components
+    mode: "light",
+    user: null, 
+    token: null,
+    posts: [] // include all the posts.
+}
+
+export const authSlice = createSlice ({
+    name: "auth", // to represent the auth workflow
+    initialState, // we are passing initial state into initial state
+    reducers: { //functions/actions that involve modifying the global state.
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin: (state, action) => { 
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        }, 
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        }, 
+        setFriends: (state, action) => { // we need to set this in local state, because we need to keep this information.
+            if(state.user) { // if the user already exists:
+                state.user.friends = action.payload.friends;
+            } else {
+                console.error("User friends non-existent.");
+            }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        }, 
+        setPost: (state, action) => { // updating a post
+            const updatedPosts = state.posts.map((post) => {
+                if(post._id === action.payload.post_id)
+                    return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPosts;
+        }
+    }
+})
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
---
+++ client/src/state/info.md
@@ -0,0 +1,2 @@
+This will be all the logics required for the application.
+This folder is where we will have redux, redux-toolkit and information. This is done to keep the data and components separate
