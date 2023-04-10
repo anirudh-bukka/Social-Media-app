af81e17f7ab28860bb59484ab7d852024f0966dd
anirudh-bukka <anirudh.bukka@gmail.com>
2023-04-10 07:59:05 +0530

    increased space between post icons

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -20,7 +20,7 @@ const HomePage = () => {
                 <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
                     <UserWidget userId={_id} picturePath={picturePath}/>
                 </Box>
-                <Box flexBasis={isNonMobileScreens ? "26%" : undefined}
+                <Box flexBasis={isNonMobileScreens ? "42%" : undefined}
                     mt={isNonMobileScreens ? undefined : "2rem"}
                 > {/* If you are on small screens, mt will be 2rem */}
                     <MyPostWidget picturePath={picturePath} />
